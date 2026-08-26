d684c59dcdecf02d56048f55093174c91b4831e0
Grant Bennett <subs@woode.co.uk>
2026-08-26 14:05:05 +0100

    fix parser test

--- tests/Unit/Irc/Protocol/MessageParserTest.php
+++ tests/Unit/Irc/Protocol/MessageParserTest.php
@@ -9,19 +9,19 @@ use PhpIrc\Irc\Protocol\MessageParser;
 use PhpIrc\Irc\Protocol\MessageTag;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
-use PHPUnit\Framework\TestCase;
+use Tests\IntegrationTestCase;
 
-final class MessageParserTest extends TestCase
+final class MessageParserTest extends IntegrationTestCase
 {
     #[Test]
     public function it_parses_a_command_and_middle_parameter(): void
     {
         $message = new MessageParser()->parse('NICK Grant');
 
-        self::assertSame([], $message->tags);
-        self::assertNull($message->source);
-        self::assertSame('NICK', $message->command);
-        self::assertSame(['Grant'], $message->parameters);
+        $this->assertSame([], $message->tags);
+        $this->assertNull($message->source);
+        $this->assertSame('NICK', $message->command);
+        $this->assertSame(['Grant'], $message->parameters);
     }
 
     #[Test]
@@ -29,8 +29,8 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse('privmsg #php :Hello everyone');
 
-        self::assertSame('PRIVMSG', $message->command);
-        self::assertSame(['#php', 'Hello everyone'], $message->parameters);
+        $this->assertSame('PRIVMSG', $message->command);
+        $this->assertSame(['#php', 'Hello everyone'], $message->parameters);
     }
 
     #[Test]
@@ -38,9 +38,9 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse(':irc.example 001 Grant :Welcome');
 
-        self::assertSame('irc.example', $message->source);
-        self::assertSame('001', $message->command);
-        self::assertSame(['Grant', 'Welcome'], $message->parameters);
+        $this->assertSame('irc.example', $message->source);
+        $this->assertSame('001', $message->command);
+        $this->assertSame(['Grant', 'Welcome'], $message->parameters);
     }
 
     #[Test]
@@ -48,7 +48,7 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse('TOPIC #php :');
 
-        self::assertSame(['#php', ''], $message->parameters);
+        $this->assertSame(['#php', ''], $message->parameters);
     }
 
     #[Test]
@@ -56,7 +56,7 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse('PRIVMSG   #php   :hello  there ');
 
-        self::assertSame(['#php', 'hello  there '], $message->parameters);
+        $this->assertSame(['#php', 'hello  there '], $message->parameters);
     }
 
     #[Test]
@@ -64,7 +64,7 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse('COMMAND abc:def ::value');
 
-        self::assertSame(['abc:def', ':value'], $message->parameters);
+        $this->assertSame(['abc:def', ':value'], $message->parameters);
     }
 
     #[Test]
@@ -74,7 +74,7 @@ final class MessageParserTest extends TestCase
             '@aaa=bbb;ccc;empty=;space=hello\sworld;semi=a\:b;slash=a\\\\b;unknown=a\qb;lone=end\\ COMMAND',
         );
 
-        self::assertEquals(
+        $this->assertEquals(
             [
                 new MessageTag('aaa', 'bbb'),
                 new MessageTag('ccc', null),
@@ -94,7 +94,7 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse('@A=first;a=lower;A=final COMMAND');
 
-        self::assertEquals(
+        $this->assertEquals(
             [
                 new MessageTag('A', 'final'),
                 new MessageTag('a', 'lower'),
@@ -110,13 +110,13 @@ final class MessageParserTest extends TestCase
             '@time=2026-08-24T12:00:00.000Z :nick!user@host PRIVMSG #php :Hello everyone',
         );
 
-        self::assertEquals(
+        $this->assertEquals(
             [new MessageTag('time', '2026-08-24T12:00:00.000Z')],
             $message->tags,
         );
-        self::assertSame('nick!user@host', $message->source);
-        self::assertSame('PRIVMSG', $message->command);
-        self::assertSame(['#php', 'Hello everyone'], $message->parameters);
+        $this->assertSame('nick!user@host', $message->source);
+        $this->assertSame('PRIVMSG', $message->command);
+        $this->assertSame(['#php', 'Hello everyone'], $message->parameters);
     }
 
     #[Test]
@@ -124,9 +124,9 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse(":nick\tname COMMAND parameter");
 
-        self::assertSame("nick\tname", $message->source);
-        self::assertSame('COMMAND', $message->command);
-        self::assertSame(['parameter'], $message->parameters);
+        $this->assertSame("nick\tname", $message->source);
+        $this->assertSame('COMMAND', $message->command);
+        $this->assertSame(['parameter'], $message->parameters);
     }
 
     #[Test]
@@ -134,7 +134,7 @@ final class MessageParserTest extends TestCase
     {
         $message = new MessageParser()->parse('@not?normally=valid COMMAND');
 
-        self::assertEquals(
+        $this->assertEquals(
             [new MessageTag('not?normally', 'valid')],
             $message->tags,
         );
