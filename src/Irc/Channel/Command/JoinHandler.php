fae6c20c7620d9c6b118d4c6c8d9f48c3c7feab4
Grant Bennett <subs@woode.co.uk>
2026-09-10 11:04:40 +0100

    improve join handler readability

--- src/Irc/Channel/Command/JoinHandler.php
+++ src/Irc/Channel/Command/JoinHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace PhpIrc\Irc\Channel\Command;
 
+use PhpIrc\Irc\Channel\Channel;
 use PhpIrc\Irc\Channel\ChannelBroadcaster;
 use PhpIrc\Irc\Channel\ChannelNameValidator;
 use PhpIrc\Irc\Channel\ChannelRegistry;
@@ -33,51 +34,71 @@ final readonly class JoinHandler implements CommandHandler
     public function handle(CommandContext $context, Message $message): void
     {
         if ($message->isParameterMissingOrEmpty(0)) {
-            $context->connection->send(
-                $this->errors->needMoreParameters($context->responseTarget(), $this->command()),
-            );
-
+            $this->sendMissingParametersResponse($context);
             return;
         }
 
-        $channels = $message->parameter(0);
-
-        foreach (explode(',', $channels) as $channelName) {
-            if (! $this->channelNames->isValid($channelName)) {
-                $context->connection->send(
-                    $this->errors->noSuchChannel($context->responseTarget(), $channelName),
-                );
-
-                continue;
-            }
-
-            $channel = $this->channels->find($channelName);
-
-            if ($channel?->hasMember($context->client)) {
-                continue;
-            }
-
-            $channel = $this->channels->join($channelName, $context->client);
-
-            $this->broadcaster->broadcast(
-                $channel,
-                new Message(
-                    command: $this->command(),
-                    parameters: [$channel->name],
-                    source: $context->actorNickname(),
-                ),
-            );
-
-            $context->connection->sendMany(
-                [
-                    ...$this->topicResponses->createExistingTopicResponses($context->responseTarget(), $channel),
-                    ...$this->namesResponses->createNamesResponses(
-                        $context->responseTarget(),
-                        $context->client,
-                        $channel,
-                    ),
-                ],
-            );
+        foreach (explode(',', $message->parameter(0)) as $channelName) {
+            $this->joinChannel($context, $channelName);
         }
+    }
+
+    private function sendMissingParametersResponse(CommandContext $context): void
+    {
+        $context->connection->send(
+            $this->errors->needMoreParameters($context->responseTarget(), $this->command()),
+        );
+    }
+
+    private function joinChannel(CommandContext $context, string $channelName): void
+    {
+        if (! $this->channelNames->isValid($channelName)) {
+            $this->sendInvalidChannelResponse($context, $channelName);
+            return;
+        }
+
+        $channel = $this->channels->find($channelName);
+
+        if ($channel?->hasMember($context->client)) {
+            return;
+        }
+
+        $channel = $this->channels->join($channelName, $context->client);
+
+        $this->broadcastJoin($context, $channel);
+        $this->sendInitialChannelState($context, $channel);
+    }
+
+    private function sendInvalidChannelResponse(CommandContext $context, string $channelName): void
+    {
+        $context->connection->send(
+            $this->errors->noSuchChannel($context->responseTarget(), $channelName),
+        );
+    }
+
+    private function broadcastJoin(CommandContext $context, Channel $channel): void
+    {
+        $this->broadcaster->broadcast(
+            $channel,
+            new Message(
+                command: $this->command(),
+                parameters: [$channel->name],
+                source: $context->actorNickname(),
+            ),
+        );
+    }
+
+    private function sendInitialChannelState(CommandContext $context, Channel $channel): void
+    {
+        $context->connection->sendMany(
+            [
+                ...$this->topicResponses->createExistingTopicResponses($context->responseTarget(), $channel),
+                ...$this->namesResponses->createNamesResponses(
+                    $context->responseTarget(),
+                    $context->client,
+                    $channel,
+                ),
+            ],
+        );
     }
 }
