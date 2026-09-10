be6f538af8d1af8e87bc87bfe9c731f2cf8c667b
Grant Bennett <subs@woode.co.uk>
2026-09-10 11:47:18 +0100

    disable temptest test discovery

--- tests/IntegrationTestCase.php
+++ tests/IntegrationTestCase.php
@@ -4,9 +4,16 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use Tempest\Discovery\DiscoveryLocation;
 use Tempest\Framework\Testing\IntegrationTest;
 
 abstract class IntegrationTestCase extends IntegrationTest
 {
     protected string $root = __DIR__ . '/../';
+
+    /** @return list<DiscoveryLocation> */
+    protected function discoverTestLocations(): array
+    {
+        return [];
+    }
 }
